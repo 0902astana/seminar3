69596baddecee4c34724f48fc9565269a162ef35
Gulmira <zhasulanovag@bk.ru>
2019-03-02 12:18:49 +0500

    privet

---
+++ linked3.py
@@ -0,0 +1,13 @@
+from linked_class import ArrayStack
+
+stak = ArrayStack()
+
+stak.push(4)
+stak.push(5)
+stak.push(6)
+stak.is_empty()
+
+stak.pop()
+
+
+print(stak.pr())
---
+++ main.py
@@ -0,0 +1,23 @@
+from myclass import Shape
+
+sh1=Shape(3,1)
+sh2=Shape(5,7)
+sh3=Shape(4,2)
+sh4=Shape()
+l=Mylist()
+l.push(sh1)
+l.push(sh2)
+l.push(sh3)
+l.sh(sh4)
+print("After adding obgects:")
+for i in l:
+    i.defene()
+
+print("\nAfter popping last objects:")
+l.pop()
+for i in l:
+    i.define()
+print("\nAfter popping last object:")
+l.pop()
+for i in l:
+    i.define()
